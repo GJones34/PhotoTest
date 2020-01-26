46af9ff547e67e2e3a11bef15f2b5fcedd06a30a
LiamAldis <laldis2089@gmail.com>
2020-01-25 22:08:06 -0800

    Merge branches 'PhotoTest_Search' and 'master' of https://github.com/GJones34/PhotoTest into PhotoTest_Search
    # Conflicts:
    #	app/src/main/java/com/example/phototest/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    
    Finished the search function

--- app/src/main/java/com/example/phototest/MainActivity.java
+++ app/src/main/java/com/example/phototest/MainActivity.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     //Initial Variables GJ
@@ -26,7 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private int currentGalleryIndex = 0;    //GJ Variable to hold position in the photo gallery
     private ArrayList<String> photoGallery; //GJ Creates a list of strings called photoGallery, used to choose which photo to display
 
+    public static final int SEARCH_ACTIVITY_REQUEST_CODE = 0;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    String SearchStartDate = "-";
+    String SearchEndDate = "-";
+    String Searchcaption = "-";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -67,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     //links the user to the search activity
     public void SearchPicture(View v) {
         Intent intent = new Intent(this, SearchActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, SEARCH_ACTIVITY_REQUEST_CODE);
     }
 
 
@@ -103,15 +109,90 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE){
-            if(resultCode == RESULT_OK) {
-                photoGallery = createGallery(new Date(), new Date());
+                SearchStartDate = data.getStringExtra("STARTDATE");
+                SearchEndDate = data.getStringExtra("ENDDATE");
+                Searchcaption = data.getStringExtra("CAPTION");
+
+                photoGallery = populateGallery();
                 currentGalleryIndex = 0;
                 mCurrentPhotoPath = photoGallery.get(currentGalleryIndex);
                 displayGallery(mCurrentPhotoPath);
             }
         }
+        if (requestCode == REQUEST_IMAGE_CAPTURE){
+            if(resultCode == RESULT_OK) {
+                photoGallery = populateGallery();
+                currentGalleryIndex = 0;
+                mCurrentPhotoPath = photoGallery.get(currentGalleryIndex);
+                displayGallery(mCurrentPhotoPath);
+            }
+        }
+    }
+
+    //Update the gallery with new search criteria
+    private ArrayList<String> populateGallery() {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/Android/data/com.example.phototest/files/Pictures");
+        photoGallery = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            boolean  SearchminDateStatus;
+            boolean  SearchmaxDateStatus;
+            boolean  SearchcaptionStatus;
+            String FileCap;
+            String Filename;
+            String Combo;
+            long Date;
+
+            for (File f : file.listFiles()) {
+                Filename = f.getName();
+                String[] separated = Filename.split("_");
+                Combo = separated[1] + separated[2];
+                Date = Long.parseLong(Combo);
+
+                if(separated.length == 5){
+                    FileCap = separated[3];
+                }else{
+                    FileCap = "";
+                }
+                //////////////////
+
+                if(SearchStartDate.equals("-")){
+                    SearchminDateStatus = true;
+                }else if(Long.parseLong(SearchStartDate) < Date){
+                    SearchminDateStatus = true;
+                }else{
+                    SearchminDateStatus = false;
+                }
+
+                if(SearchEndDate.equals("-")){
+                    SearchmaxDateStatus = true;
+                }else if(Long.parseLong(SearchEndDate) > Date){
+                    SearchmaxDateStatus = true;
+                }else{
+                    SearchmaxDateStatus = false;
+                }
+
+                if(Searchcaption.equals("-")){
+                    SearchcaptionStatus = true;
+                }else if(FileCap.toLowerCase().contains(Searchcaption.toLowerCase())){
+                    SearchcaptionStatus = true;
+                }else{
+                    SearchcaptionStatus = false;
+                }
+
+                if((SearchminDateStatus == true)&&(SearchmaxDateStatus == true)&&(SearchcaptionStatus == true)){
+                    photoGallery.add(f.getPath());
+                }
+
+                //////////////////
+
+            }
+        }
+        return photoGallery;
     }
 
     //GJ Display the photo on the screen
