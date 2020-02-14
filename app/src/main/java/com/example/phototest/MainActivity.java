6965853ba305eefe62839cb64631756e6ba3f909
LiamAldis <laldis2089@gmail.com>
2020-02-14 13:14:34 -0800

    Finished the Location Search
    Started the Upload code
    
    Finished the Upload code
    Edited the main page layout
    Added internet permissions

--- app/src/main/java/com/example/phototest/MainActivity.java
+++ app/src/main/java/com/example/phototest/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.FileProvider;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
